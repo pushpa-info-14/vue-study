123f8ded71add1055f7be959713850387ebeca0a
pushpa.info.14 <pushpa.info.14@gmail.com>
2025-03-14 21:46:35 +0530

    initial

---
+++ src/components/ModelBinding/InputComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div>
+      <label for="input">Text Input</label>
+      <input type="text" v-model="textInputValue" />
+      <p>
+        Text Input Output: <span>{{ textInputValue }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script type="ts">
+import { ref } from 'vue';
+
+const textInputValue = ref("");
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <label for="input">Text Input</label>
+      <!-- <input type="text" v-model="textInputValue" /> -->
+      <input
+        type="text"
+        :value="textInputValue"
+        @input="textInputValue = ($event.target as HTMLInputElement).value"
+      />
+      <p>
+        Text Input Output: <span>{{ textInputValue }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const textInputValue = ref('')
+</script>
